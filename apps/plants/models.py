901afef7418d8f35ac6757202fcab0ca2f1e9d23
ollz272 <46482091+ollz272@users.noreply.github.com>
2022-02-19 14:07:48 +0000

    Adds project setup

---
+++ apps/plants/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+from django.utils.text import slugify
+
+
+class Plant(models.Model):
+    # TODO.
+    name = models.CharField(max_length=256)
+    indoor = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name, allow_unicode=True)
+        return super(Plant, self).save(*args, **kwargs)
+
+
+class DataPoint(models.Model):
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField(null=True)
+    light_level = models.FloatField(null=True)
+    moisture_level = models.FloatField(null=True)
