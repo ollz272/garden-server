3d4a3465c1596003d1f6eef85fc8bd1b85f96365
Oliver Parker <oliver@dev.ngo>
2022-06-19 10:01:29 +0100

    fixes

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from accounts.views import UserCreateView
+
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("signup/", UserCreateView.as_view(), name="signup")
 ]
