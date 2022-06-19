d2bc4c99651f47f6bf1662dbbe57109e39057cd0
Oliver Parker <oliver@dev.ngo>
2022-06-19 10:24:12 +0100

    Fixes

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
-(gunicorn project.wsgi --user www-data --bind 0.0.0.0:8010 --workers 10) &
+(gunicorn project.wsgi --user www-data --bind 0.0.0.0:8010 --workers 4) &
 nginx -g "daemon off;"
