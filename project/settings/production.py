18fe57179b4cfe1ef61ae2ebc56869a6ff7405ca
Oliver Parker <oliver@dev.ngo>
2022-06-19 10:36:27 +0100

    Fixes CSFR origins

--- project/settings/production.py
+++ project/settings/production.py
@@ -2,5 +2,6 @@ from .base import *
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST', '').split(' ')
 DEBUG= True
-#CSRF_TRUSTED_ORIGINS = [f"http://{os.environ.get('ALLOWED_HOST', '').split(' ')}"]
+
+CSRF_TRUSTED_ORIGINS = [f"http://{host}" for host in os.environ.get('ALLOWED_HOST', '').split(' ')]
 SECRET_KEY = 'gardenserver'
