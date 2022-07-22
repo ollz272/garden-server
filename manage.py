78796733bc2913851f9c8a637a02369ddaebdfd0
Oliver Parker <oliver@dev.ngo>
2022-07-22 14:33:24 +0100

    Fixes! hopefully

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    print(os.environ.get('DJANGO_SETTINGS_MODULE'))
+    os.environ.get('DJANGO_SETTINGS_MODULE', 'project.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
