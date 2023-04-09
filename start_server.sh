e3cc55cfd61c4bedf067518fe2349ef27e7004fb
Oli <oli.edmund.parker@gmail.com>
2023-04-09 21:21:16 +0100

    fix: docker-image

--- start_server.sh
+++ start_server.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Migrating.."
-poetry run ./manage.py migrate --settings="project.settings.production"
+./manage.py migrate --settings="project.settings.production"
 
 
 # start-server.sh
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (poetry run ./manage.py createsuperuser --no-input)
+    (./manage.py createsuperuser --no-input)
 fi
-(poetry run gunicorn project.wsgi --user www-data --bind 0.0.0.0:8010 --workers 4) &
+(gunicorn project.wsgi --user www-data --bind 0.0.0.0:8010 --workers 4) &
 nginx -g "daemon off;"
