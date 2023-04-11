ab609f392bde48f4f62f03c74f472db484e99421
Oli <oli.edmund.parker@gmail.com>
2023-04-11 07:50:57 +0100

    fix: restores production file to former glory

--- project/settings/production.py
+++ project/settings/production.py
@@ -8,7 +8,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 if os.environ.get("DEBUG"):
     DEBUG = True
 
-
 DATABASES = {
     "default": {
         "ENGINE": "timescale.db.backends.postgresql",
@@ -25,22 +24,13 @@ DATABASES = {
 if os.environ.get("REDIS_SERVERS"):
     SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
-
-if not os.environ.get("DISABLE_TOOLBAR"):
-    CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1"]
-    ALLOWED_HOSTS = ["*"]
-    INSTALLED_APPS += [
-        "debug_toolbar",
+# Cache pages to a bare minimum to ensure decent performance under high load
+MIDDLEWARE = (
+    [
+        "cookiefilter.middleware.CookieFilterMiddleware",
+        "django.middleware.cache.UpdateCacheMiddleware",
     ]
-
-    MIDDLEWARE = [
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-    ] + MIDDLEWARE
-
-    DEBUG_TOOLBAR_CONFIG = {
-        "SKIP_TEMPLATE_PREFIXES": (
-            "django/forms/widgets/",
-            "admin/widgets/",
-            "bootstrap/",
-        ),
-    }
+    + MIDDLEWARE
+    + ["django.middleware.cache.FetchFromCacheMiddleware"]
+)
+CACHE_MIDDLEWARE_SECONDS = 30
