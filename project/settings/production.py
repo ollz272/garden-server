4113632c873f4a7d9b6730b7565784afed41d90c
Oli <oli.edmund.parker@gmail.com>
2023-04-11 07:42:14 +0100

    chore: update deployment issues

--- project/settings/production.py
+++ project/settings/production.py
@@ -8,7 +8,27 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 if os.environ.get("DEBUG"):
     DEBUG = True
 
+
+DATABASES = {
+    "default": {
+        "ENGINE": "timescale.db.backends.postgresql",
+        "NAME": os.environ.get("DJANGO_DATABASE_NAME", "gardenserver_django"),
+        "USER": os.environ.get("DJANGO_DATABASE_USER"),
+        "PASSWORD": os.environ.get("DJANGO_DATABASE_PASSWORD"),
+        "PORT": "5432",
+        "HOST": os.environ.get("DJANGO_DATABASE_HOST"),
+    },
+}
+
+
+# Cache sessions for optimum performance
+if os.environ.get("REDIS_SERVERS"):
+    SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
+
 if not os.environ.get("DISABLE_TOOLBAR"):
+    CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1"]
+    ALLOWED_HOSTS = ["*"]
     INSTALLED_APPS += [
         "debug_toolbar",
     ]
@@ -24,34 +44,3 @@ if not os.environ.get("DISABLE_TOOLBAR"):
             "bootstrap/",
         ),
     }
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "timescale.db.backends.postgresql",
-        "NAME": os.environ.get("DJANGO_DATABASE_NAME", "gardenserver_django"),
-        "USER": os.environ.get("DJANGO_DATABASE_USER"),
-        "PASSWORD": os.environ.get("DJANGO_DATABASE_PASSWORD"),
-        "PORT": "5432",
-        "HOST": os.environ.get("DJANGO_DATABASE_HOST"),
-    },
-}
-
-
-GS_BUCKET = "gardenserver_bucket"
-GS_PROJECT_ID = "gardenserver"
-
-# Cache sessions for optimum performance
-if os.environ.get("REDIS_SERVERS"):
-    SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-
-# Cache pages to a bare minimum to ensure decent performance under high load
-MIDDLEWARE = (
-    [
-        "cookiefilter.middleware.CookieFilterMiddleware",
-        "django.middleware.cache.UpdateCacheMiddleware",
-    ]
-    + MIDDLEWARE
-    + ["django.middleware.cache.FetchFromCacheMiddleware"]
-)
-CACHE_MIDDLEWARE_SECONDS = 30
