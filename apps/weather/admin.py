350df13c8ab8920552ad20e1d3cbddc8b08aed61
Oli <oli.edmund.parker@gmail.com>
2023-04-18 19:25:41 +0100

    fix: adds some fixes to the weather model and admin

--- apps/weather/admin.py
+++ apps/weather/admin.py
@@ -5,5 +5,23 @@ from .models import Weather
 
 @admin.register(Weather)
 class WeatherAdmin(admin.ModelAdmin):
-    list_filter = ["date_time", "location", "is_forecast"]
-    read_only_fields = ["is_forecast", "created", "updated"]
+    list_filter = ["date_time", "location", "is_forecast", "weather_code"]
+    readonly_fields = ["is_forecast", "created", "updated"]
+    list_display = ["date_time", "location", "is_forecast", "weather_code"]
+
+    fieldsets = [
+        (
+            "Weather Info",
+            {
+                "fields": ["date_time", "location"],
+            },
+        ),
+        ("Weather Data", {"fields": ["temperature", "apparent_temperature", "rain", "cloud_cover", "weather_code"]}),
+        (
+            "Meta Data",
+            {
+                "classes": ["collapse"],
+                "fields": ["is_forecast", "created", "updated"],
+            },
+        ),
+    ]
