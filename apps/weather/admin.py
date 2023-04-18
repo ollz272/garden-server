e8403ea408d83836ee1b3d45c4a80af36a110f33
Oli <oli.edmund.parker@gmail.com>
2023-04-18 08:58:12 +0100

    feat: add weather admin, add weather str

--- apps/weather/admin.py
+++ apps/weather/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Weather
+
+
+@admin.register(Weather)
+class WeatherAdmin(admin.ModelAdmin):
+    list_filter = ["date_time", "location"]
