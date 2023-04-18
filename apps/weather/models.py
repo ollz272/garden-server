4361115ff5e1afd06ebab922232f913a4ea33817
Oliver Parker <46482091+ollz272@users.noreply.github.com>
2023-04-18 19:30:04 +0100

    fix: adds some fixes to the weather model and admin (#215)
    * fix: adds some fixes to the weather model and admin
    
    * fix: add missing migrations and choices

--- apps/weather/models.py
+++ apps/weather/models.py
@@ -1,6 +1,8 @@
 from django.contrib.gis.db import models as gis_models
 from django.db import models
 
+from weather import choices
+
 # Create your models here.
 
 
@@ -20,16 +22,21 @@ class Weather(models.Model):
     apparent_temperature = models.FloatField()
     rain = models.FloatField()
     cloud_cover = models.FloatField()
-    weather_code = models.FloatField()  # TODO there are choices.
+    weather_code = models.FloatField(choices=choices.WeatherTypes.choices)
 
     is_forecast = models.BooleanField()
-
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(name="unique_weather_for_location_time", fields=["date_time", "location"])
+        ]
+        indexes = [
+            models.Index(fields=["date_time"]),
+            models.Index(fields=["location"]),
+            models.Index(fields=["is_forecast"]),
+            models.Index(fields=["weather_code"]),
         ]
 
     def __str__(self):
